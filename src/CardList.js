8999a4228d55ce6a8d6c1edc134e29195d71f22f
Geetha Yodham <geethayodham@gmail.com>
2020-06-03 16:52:40 -0500

    Refatored some code

--- src/CardList.js
+++ src/CardList.js
@@ -3,14 +3,23 @@ import React from 'react';
 import Card from './Card'
 
 const CardList = ({upcs}) => { //{upcs} = const {upcs} = props.upcs
-    const cardComponent = upcs.map ((user, i) => {
-       return  <Card id={upcs[i].id} productname={upcs[i].productname} upc={upcs[i].upc}/>
-    })
+
     return (
         <div>
-            {cardComponent}
+            {
+                upcs.map((user, i) => {
+                    return (
+                        <Card
+                            key={i}
+                            id={upcs[i].id}
+                            productname={upcs[i].productname}
+                            upc={upcs[i].upc}
+                        />
+                    );
+                })
+            }
         </div>
-    )
+    );
 }
 
 export default CardList;
