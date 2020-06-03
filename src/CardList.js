8d7b6fc0600a36ad7c4816c337953556f1cb5083
Geetha Yodham <geethayodham@gmail.com>
2020-06-03 16:19:21 -0500

    Replace static array code to Map

--- src/CardList.js
+++ src/CardList.js
@@ -3,11 +3,12 @@ import React from 'react';
 import Card from './Card'
 
 const CardList = ({upcs}) => { //{upcs} = const {upcs} = props.upcs
+    const cardComponent = upcs.map ((user, i) => {
+       return  <Card id={upcs[i].id} productname={upcs[i].productname} upc={upcs[i].upc}/>
+    })
     return (
         <div>
-            <Card id={upcs[0].id} productname={upcs[0].productname} upc={upcs[0].upc}/>
-            <Card id={upcs[1].id} productname={upcs[1].productname} upc={upcs[1].upc}/>
-            <Card id={upcs[2].id} productname={upcs[2].productname} upc={upcs[2].upc}/>
+            {cardComponent}
         </div>
     )
 }
