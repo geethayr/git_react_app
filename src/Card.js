f5efaa45b21811348ab5dee4b4be103e0e018f6a
Geetha Yodham <geethayodham@gmail.com>
2020-06-03 16:04:09 -0500

    Es6 Destructed props

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({id, productname, upc }) => { //destructed 
+    //const {id, productname, upc } = props
     return (
         <div className='tc bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-2'>
             <img alt='Barcode Generator TEC-IT'
-                 src='https://barcode.tec-it.com/barcode.ashx?data=72527273070&code=UPCA&multiplebarcodes=false&translate-esc=false&unit=Fit&dpi=96&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=0'/>
+                 src={`https://barcode.tec-it.com/barcode.ashx?data=${upc}&code=UPCA&multiplebarcodes=false&translate-esc=false&unit=Fit&dpi=96&imagetype=Gif&rotation=0&color=%23000000&bgcolor=%23ffffff&codepage=&qunit=Mm&quiet=0`}/>
             <div>
-                <h2>{props.productname}</h2>
-                <p>{props.upc}</p>
+                <h2>{productname}</h2>
+                <p>{upc}</p>
             </div>
         </div>
     );
