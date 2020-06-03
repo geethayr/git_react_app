d6bd8bec4e7a9d180e6cde78099decc06ee640d3
Geetha Yodham <geethayodham@gmail.com>
2020-06-03 17:50:53 -0500

    onChange call from App to SearcgBox

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {upcs} from "./upcs";
 import SearchBox from './SearchBox';
 import CardList from './CardList';
 
 
-const App = () => {
-    return(
-        <div>
-            <h1>Returns TC decoder</h1>
-            <SearchBox/>
-            <CardList upcs={upcs}/>
-        </div>
-    );
-}
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            upcs: upcs,
+            searchfield: ''
+        }
+    }
 
+    onSearchChange(event) {
+        console.log("[Event in App]", event.target.value)
+
+    }
+    render() {
+        return(
+            <div className='tc'>
+                <h1>Returns TC decoder</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList upcs={this.state.upcs}/>
+                </div>
+        );
+    }
+}
 
 export default App;
