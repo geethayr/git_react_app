be6ed9b0eb0782be095d43da183ca65138684ccc
Geetha Yodham <geethayodham@gmail.com>
2020-06-04 09:27:55 -0500

    Added Sega.woff and App.css for styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {upcs} from "./upcs";
 import SearchBox from './SearchBox';
 import CardList from './CardList';
+import './App.css'
 
 
 class App extends Component {
@@ -21,12 +22,12 @@ class App extends Component {
     }
     render() {
         const filteredUpc = this.state.upcs.filter(upcs => {
-            return upcs.productname.toLowerCase().includes(this.state.searchfield.toLowerCase());
+            return upcs.upc.toLowerCase().includes(this.state.searchfield.toLowerCase());
             console.log("[Filtered UPCS in App]", filteredUpc);
         })
         return(
             <div className='tc'>
-                <h1>Returns TC decoder</h1>
+                <h1 className='f2'>BarCode Generator</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList upcs={filteredUpc}/>
                 </div>
