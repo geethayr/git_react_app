c99152bc971574396d98f4b77e7976989fbfdf8f
Geetha Yodham <geethayodham@gmail.com>
2020-06-15 17:01:26 -0500

    Updates

---
+++ precache-manifest.07c3de22daa31ec619fa85e3e2d7bfd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8125aa359453bd1433a97b592faef14",
+    "url": "/git_react_app/index.html"
+  },
+  {
+    "revision": "bfe02f5e3ff94ed7dbba",
+    "url": "/git_react_app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d62b23362cb33abd9bef",
+    "url": "/git_react_app/static/css/main.a9971683.chunk.css"
+  },
+  {
+    "revision": "bfe02f5e3ff94ed7dbba",
+    "url": "/git_react_app/static/js/2.9f5e6382.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git_react_app/static/js/2.9f5e6382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d62b23362cb33abd9bef",
+    "url": "/git_react_app/static/js/main.3d89d224.chunk.js"
+  },
+  {
+    "revision": "9acc0541c4044ff03aea",
+    "url": "/git_react_app/static/js/runtime-main.f0d0e69d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/git_react_app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
